dac9f5fade5cbc502e7424147426cc09478c7c46
PaulQSC <74190029+PaulQSC@users.noreply.github.com>
2020-11-10 15:28:10 +0100

    Update and rename init-dok.txt to init-dok.js

---
+++ init-dok.js
@@ -0,0 +1,25 @@
+var SpecialModeResponse = {
+    'N/A':                  '',
+    'POWERFUL':             '2',
+	'ECONO':                '12',
+	'STREAMER':             '13',
+    'POWERFUL/STREAMER':    '2/13',
+    'ECONO/STREAMER':       '12/13'
+};
+
+var FanRate = {
+	'AUTO':    'A',
+	'SILENCE': 'B',
+	'LEVEL_1': 3,
+	'LEVEL_2': 4,
+	'LEVEL_3': 5,
+	'LEVEL_4': 6,
+	'LEVEL_5': 7
+};
+
+var FanDirection = {
+	'STOP':       0,
+	'VERTICAL':   1,
+	'HORIZONTAL': 2,
+	'VERTICAL_AND_HORIZONTAL': 3
+};
